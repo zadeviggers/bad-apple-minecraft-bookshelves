a7a093662b9409e3b7dba3fd63507384161c926d
Zade Viggers <zade@viggers.net>
2022-11-18 21:29:37 +1300

    Format

--- generate-function.py
+++ generate-function.py
@@ -1,19 +1,20 @@
 import json
-  
+
 print("opening json file...")
 f = open("output.data")
 print("file opened!")
-  
+
 data = json.load(f)
 print("data loaded")
 f.close()
 print("file closed")
 
-#setblock ~-1 ~ ~ minecraft:chiseled_bookshelf[facing=east, slot_0_occupied=true]
+# setblock ~-1 ~ ~ minecraft:chiseled_bookshelf[facing=east, slot_0_occupied=true]
 
 frame_count = len(data)
 
 print("generating functions")
+
 
 def bool_str(thing: bool) -> str:
     if thing:
@@ -49,12 +50,13 @@ for frame_number, frame in enumerate(data):
 
             top_middle = top_left or top_right
             bottom_middle = bottom_left or bottom_right
-            
-            slots = [bool_str(top_left), bool_str(top_middle), bool_str(top_right), bool_str(bottom_left), bool_str(bottom_middle), bool_str(bottom_right)]
+
+            slots = [bool_str(top_left), bool_str(top_middle), bool_str(top_right), bool_str(
+                bottom_left), bool_str(bottom_middle), bool_str(bottom_right)]
             # White pixels are true
 
             command = f"""setblock 0 {-60 + height - i} {j} minecraft:chiseled_bookshelf[facing=east, slot_0_occupied={slots[0]}, slot_1_occupied={slots[1]}, slot_2_occupied={slots[2]}, slot_3_occupied={slots[3]}, slot_4_occupied={slots[4]}, slot_5_occupied={slots[5]}]"""
-            
+
             commands_length = len(commands)
             if commands_length <= 2 or command != commands[commands_length - 1]:
                 commands.append(command)
@@ -72,9 +74,10 @@ for frame_number, frame in enumerate(data):
 
     #             command = f"""setblock 0 {-60 + height - i} {j} minecraft:chiseled_bookshelf[facing=east, slot_0_occupied={slots[0]}, slot_1_occupied={slots[1]}, slot_2_occupied={slots[2]}, slot_3_occupied={slots[3]}, slot_4_occupied={slots[4]}, slot_5_occupied={slots[5]}]"""
     #             commands.append(command)
-   
+
     if frame_number != frame_count - 1:
-        commands.append(f"schedule function bad-apple:frames/frame-{frame_number + 1} 2t")
+        commands.append(
+            f"schedule function bad-apple:frames/frame-{frame_number + 1} 2t")
 
     with open(f"datapack/data/bad-apple/functions/frames/frame-{frame_number}.mcfunction", "w") as outfile:
         outfile.write("\n".join(commands))

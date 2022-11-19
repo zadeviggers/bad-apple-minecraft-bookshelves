66569380fb82bb4cf4bae890b3ea46a09e88317d
Zade Viggers <zade@viggers.net>
2022-11-19 15:27:50 +1300

    It kinda works

--- generate_function.py
+++ generate_function.py
@@ -2,6 +2,11 @@ import json
 import math
 from typing import Optional
 
+command_start = "setblock "
+command_start_len = len(command_start)
+
+height_offset = 50
+
 print("opening json file...")
 f = open("output.data", encoding="utf-8")
 print("file opened!")
@@ -25,9 +30,23 @@ def get_colour_or_black(things: list, i: int, j: int) -> str:
         return "false"
 
 
-prev_frame_commands: Optional[set] = None
+def de_dupe_functions(functions: list[str], to_check: str) -> Optional[str]:
+    for function in reversed(functions):
+        try:
+            coords_index = function.index(to_check)
+            start_index = coords_index - command_start_len
+            end_index = function.index("\n", coords_index)
+            substr = function[start_index:end_index]
+            return substr
+        except ValueError:
+            pass
+    return None
+
+
+functions: list[str] = []
+
 for frame_number, frame in enumerate(data):
-    commands = set()
+    commands = []
     height = len(frame)
 
     books = []
@@ -63,7 +82,7 @@ for frame_number, frame in enumerate(data):
             #     # Dark grey
             #     slots = [
             #         ["true", "true"],
-            #         ["true", "true"]
+            #         ["false", "true"]
             #     ]
 
             books[-2].append(slots[0][0])
@@ -90,21 +109,23 @@ for frame_number, frame in enumerate(data):
             slots.append(get_colour_or_black(books, i + 2, j + 1))
             slots.append(get_colour_or_black(books, i + 3, j + 2))
 
-            command = f'setblock 0 {math.ceil((-60 + height - i) / 2)} {math.ceil(j / 3)} minecraft:chiseled_bookshelf[facing=west, slot_0_occupied={slots[0]}, slot_1_occupied={slots[1]}, slot_2_occupied={slots[2]}, slot_3_occupied={slots[3]}, slot_4_occupied={slots[4]}, slot_5_occupied={slots[5]}]'
+            coordinates = f"0 {math.ceil((-60 + height - i + height_offset) / 2)} {math.ceil(j / 3)}"
+            command = f'{command_start}{coordinates} minecraft:chiseled_bookshelf[facing=west, slot_0_occupied={slots[0]}, slot_1_occupied={slots[1]}, slot_2_occupied={slots[2]}, slot_3_occupied={slots[3]}, slot_4_occupied={slots[4]}, slot_5_occupied={slots[5]}]'
 
-            # Don't need to update the block if it was updated the same way last frame
-            if (prev_frame_commands is not None) and (command not in prev_frame_commands):
-                commands.add(command)
+            # Don't need to update the block if it was updated to be the same last time it was touched
+            if len(functions) == 0 or de_dupe_functions(functions, coordinates) != command:
+                commands.append(command)
 
-    prev_frame_commands = commands
+    output = "\n".join(commands)
+    if frame_number != frame_count - 1:
+        output += f"\nexecute if score playing bad_apple matches 1 run schedule function bad-apple:frames/frame-{frame_number + 1} 2t"
 
     with open(f"datapack/data/bad-apple/functions/frames/frame-{frame_number}.mcfunction", "w",
               encoding="utf-8") as outfile:
-        output = "\n".join(commands)
-        if frame_number != frame_count - 1:
-            output += f"\nexecute if score playing bad_apple matches 1 run schedule function bad-apple:frames/frame-{frame_number + 1} 2t"
 
         outfile.write(output)
+
+    functions.append(output)
 
     if frame_number % 100 == 0:
         print(f"generated function {frame_number}")
